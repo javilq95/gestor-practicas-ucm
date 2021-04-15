44092eddadb7b961cded45256e1a488d2a120830
Javier López Quesada <47363829+javilq95@users.noreply.github.com>
2021-04-15 21:07:11 +0200

    Lead incluido en formulario

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Lead } from 'src/app/salesforce/Lead.model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  model = new Lead('Javier','example@gmail.com','');
+
   constructor() { }
 //
   ngOnInit() {
+  }
+
+  get currentLead(){
+    return JSON.stringify(this.model);
   }
 
 }
---
+++ src/app/salesforce/lead.model.ts
@@ -0,0 +1,7 @@
+export class Lead {
+    constructor(
+        public name: string,
+        public email: string,
+        public password: string
+    ){}
+}
