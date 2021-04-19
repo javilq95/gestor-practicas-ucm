ffd31a521f391136aed91dd035f366833bd6098c
Javier López Quesada <47363829+javilq95@users.noreply.github.com>
2021-04-19 23:22:02 +0200

    Integración con salesforce v1

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Lead } from 'src/app/salesforce/Lead.model';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-register',
@@ -8,15 +9,31 @@ import { Lead } from 'src/app/salesforce/Lead.model';
 })
 export class RegisterComponent implements OnInit {
 
-  model = new Lead('Javier','example@gmail.com','');
+  model = new Lead('','','');
 
-  constructor() { }
+  public s;
+
+  constructor(private http: HttpClient) { }
 //
   ngOnInit() {
+    this.getapi();
   }
 
   get currentLead(){
     return JSON.stringify(this.model);
+  }
+
+  public async getapi(){
+    return await this.http.get<any>(
+      'https://wam-dev-ed.my.salesforce.com/services/data/v42.0/query/?q=SELECT+Name+FROM+Account',
+        {
+            headers: {
+              'Authorization': 'Bearer 00D09000007iY11!AREAQKGxJhAPyKMz3TlNg4Ji6_Hxvisjh_uADX9RpAWItjJHmNpP2LaKwFMzXesnFyA7BrwlUde1P8ReWXcUVG_R1ts6q5UI',
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'GET'
+            }
+        }).toPromise().then(x => this.s = x);
+    //return this.http.get('Bearer https://wam-dev-ed.my.salesforce.com/services/data/v42.0/query/?q=SELECT+Name+FROM+Account');
   }
 
 }
