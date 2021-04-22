eb43e8520aaee85c6e63078b45f06c2062b6c0be
Javier López Quesada <47363829+javilq95@users.noreply.github.com>
2021-04-22 21:23:27 +0200

    Integración con salesforce v3

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -9,52 +9,54 @@ import { HttpClient } from "@angular/common/http";
 })
 export class RegisterComponent implements OnInit {
 
-  model = new Lead('','','');
+  model = new Lead('', '', '');
 
   public s;
 
   constructor(private http: HttpClient) { }
-//
+  //
   ngOnInit() {
     this.authSalesforce();
   }
 
-  get currentLead(){
+  get currentLead() {
     return JSON.stringify(this.model);
   }
 
-  public async authSalesforce(){
+  public async authSalesforce() {
+
+    let body = {
+      'username': 'prsucm@gmail.com',
+      'password': 'eTiMAlM8QqtnXZSisrr8Uroge',
+      'grant_type': 'password',
+      'client_id': '3MVG9SOw8KERNN08z_oVBiDovu6mJpSAiAXITiJ4ViMs8DPEh_PPXnf9sdNhCyT.TGM9TN7ucJxfIB_mJk4D9',
+      'client_secret': 'A447A22720FC315D2338C4066AA5785F56B4BFAFAAE22870E7CDE6203241F76B'
+    };
 
     return await this.http.post<any>(
       'https://login.salesforce.com/services/oauth2/token',
-        {
-          body: {
-            'username': 'prsucm@gmail.com',
-            'password': 'eTiMAlM8QqtnXZSisrr8Uroge',
-            'grant_type': 'password',
-            'client_id': '3MVG9SOw8KERNN08z_oVBiDovu6mJpSAiAXITiJ4ViMs8DPEh_PPXnf9sdNhCyT.TGM9TN7ucJxfIB_mJk4D9',
-            'client_secret': 'A447A22720FC315D2338C4066AA5785F56B4BFAFAAE22870E7CDE6203241F76B',
-          },
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST'
-          }
-        }).toPromise().then(x => this.s = x.access_token);
-        
+      body,
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST'
+        }
+      }).toPromise().then(x => this.s = x.access_token);
+
   }
 
 
-  public async getAPI(){
+  public async getAPI() {
 
     return await this.http.get<any>(
       'https://wam-dev-ed.my.salesforce.com/services/data/v42.0/query/?q=SELECT+Name+FROM+Lead',
-        {
-            headers: {
-              'Authorization': 'Bearer 00D09000007iY11!AREAQJ2emevFtXJupD1gmvAyZ_gEqvWEl66yoio_dETg8aevKWR6schhCv84hPe91f0ZfIBlcd3jQCdH0noC6VSE9Af2HB7K',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Methods': 'GET'
-            }
-        }).toPromise().then(x => this.s = JSON.stringify(x));
+      {
+        headers: {
+          'Authorization': 'Bearer 00D09000007iY11!AREAQJ2emevFtXJupD1gmvAyZ_gEqvWEl66yoio_dETg8aevKWR6schhCv84hPe91f0ZfIBlcd3jQCdH0noC6VSE9Af2HB7K',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET'
+        }
+      }).toPromise().then(x => this.s = JSON.stringify(x));
 
   }
 
