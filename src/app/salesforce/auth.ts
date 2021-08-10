e0fcb70fbab6d1a69975fd66ec5e2163aa617bc5
Javier López Quesada <47363829+javilq95@users.noreply.github.com>
2021-08-10 21:34:37 +0200

    Primera modificación de la interfaz general

--- src/app/salesforce/auth.ts
+++ src/app/salesforce/auth.ts
@@ -1,2 +1,4 @@
-export var accessToken = "Bearer 00D09000007iY11!AREAQNRnBOfzw9IPOgIh4lgAuRdmyMZGpibvIxvSWON_5mbM8r3PCpL59pid7vmcyLUpYsJLAM7JddkZgssmV3iaO8hT0d8D";
-export var currentEmailLead = "";
+export var accessToken = "Bearer 00D09000007iY11!AREAQGy2c36uaIAYqTiQMl8QYTV0_GPyLAWtCRl5fmC57qsKCucKquaDDV6N6HPg1lk9aOtmA5l_Q7dG0BvmXxdFID79SZdh";
+export var currentEmail = "";
+export var currentId = "";
+export var currentType = "";
