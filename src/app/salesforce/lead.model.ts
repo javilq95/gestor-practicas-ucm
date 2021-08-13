5c78e84dd7bce014b28dbea3b1d709ca0a2c5d97
Javier López Quesada <47363829+javilq95@users.noreply.github.com>
2021-08-13 22:57:03 +0200

    Update y reset en lead

--- src/app/salesforce/lead.model.ts
+++ src/app/salesforce/lead.model.ts
@@ -1,8 +1,9 @@
 import { HttpClient} from "@angular/common/http";
 import { accessToken } from "./auth";
 export class Lead {
-    public s: string;
-    public id: string;
+    public s: string = null;
+    public id: string = null;
+    public error: string = null;
     constructor(
         public firstName: string,
         public lastName: string,
@@ -43,13 +44,40 @@ export class Lead {
             }).toPromise().then(x => this.s = JSON.stringify(x));
     }
 
+    public async updateLeadSF() {
+
+      var body = {
+          'FirstName':this.firstName, 
+          'LastName': this.lastName, 
+          'Email': this.email,
+          'Password__c': this.password,
+          'NIF__c': this.nif,
+          'Area__c': this.area,
+          'Titulation__c': this.titulation,
+          'Phone': this.phone,
+          'City': this.city,
+          'Company': 'Universidad Complutense de Madrid',
+      };
+      return this.http.post<any>(
+          'https://wam-dev-ed.my.salesforce.com/services/data/v49.0/sobjects/lead',
+          body,
+          {
+            headers: {
+              'Authorization': accessToken,
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'POST',
+              'Content-Type': 'application/json'
+            }
+          }).toPromise().then(x => this.s = JSON.stringify(x));
+  }
+
     get currentLead() {
         return JSON.stringify(this);
     }
 
     public async loginLeadSF (loginEmail:string, loginPassword:string){
 
-        var endPoint = "https://wam-dev-ed.my.salesforce.com/services/data/v42.0/query/?q=SELECT+Id+,+FirstName+,+LastName+,+Email+,+Password__c+,+NIF__c+,+Area__c+,+Titulation__c+,+Phone+,+City+,+Company+FROM+Lead+WHERE+Email='"+loginEmail+"'+AND+Password__c='"+loginPassword+"'";
+        var endPoint = "https://wam-dev-ed.my.salesforce.com/services/data/v42.0/query/?q=SELECT+Id+,+FirstName+,+LastName+,+Email+,+Password__c+,+NIF__c+,+Area__c+,+Titulation__c+,+Phone+,+City+,+Company+FROM+Lead+WHERE+Email='"+loginEmail+"'+AND+Password__c='"+loginPassword+"'+AND+Verification__c=true";
 
         await this.http.get<any>(
             endPoint,
@@ -64,6 +92,7 @@ export class Lead {
             var parsed = JSON.parse(this.s);
 
             if (parsed.totalSize == 1){
+              this.id = parsed.records[0].Id;
               this.firstName = parsed.records[0].FirstName;
               this.lastName = parsed.records[0].LastName;
               this.email = parsed.records[0].Email;
@@ -73,6 +102,21 @@ export class Lead {
               this.titulation = parsed.records[0].Titulation__c;
               this.phone = parsed.records[0].Phone;
               this.city = parsed.records[0].City;
+            }else{
+              this.error = "Error, los credenciales no son correctas o el alumno está verificado";
             }
+    }
+
+    public resetObject(){
+      this.id = null;
+      this.firstName = null;
+      this.lastName = null;
+      this.email = null;
+      this.password = null;
+      this.nif = null;
+      this.area = null;
+      this.titulation = null;
+      this.phone = null;
+      this.city = null;
     }
 }
